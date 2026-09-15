ac1e7e387f0a6a3a4ba78a7d4789ffd67037ae43
ABCD123 <anasanwer46@gmail.com>
2026-09-15 12:33:10 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import PlausibleProvider from "next-plausible";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -52,11 +53,10 @@ export default function RootLayout({
       <head>
         <PlausibleProvider domain="roasterai.vercel.app" />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+  {children}
+  <Analytics />
+</body>
     </html>
   );
 }
