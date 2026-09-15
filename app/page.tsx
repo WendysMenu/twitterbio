7e6e3cd8c8e42f907c25712e87ec6762cc5ed702
ABCD123 <anasanwer46@gmail.com>
2026-09-15 16:17:40 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,13 @@ function NativeAd() {
 }
 
   return (
+    <div className="my-6 flex justify-center items-center">
+      <div id="container-2e7082e789ee952c6c92ca80807feec2"></div>
+    </div>
+  );
+}
+
+  return (
     <div className="my-6 flex justify-center items-center min-h-[90px]">
       <div id="container-2e7082e789ee952c6c92ca80807feec2"></div>
     </div>
