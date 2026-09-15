930978b1c3ab06ef7282acc4eb97ca3466ad925c
ABCD123 <anasanwer46@gmail.com>
2026-09-15 16:20:47 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect } from "react";
+
 function NativeAd() {
   useEffect(() => {
     const script = document.createElement('script');
@@ -15,29 +16,6 @@ function NativeAd() {
     </div>
   );
 }
-
-  return (
-    <div className="my-6 flex justify-center items-center">
-      <div id="container-2e7082e789ee952c6c92ca80807feec2"></div>
-    </div>
-  );
-}
-
-  return (
-    <div className="my-6 flex justify-center items-center min-h-[90px]">
-      <div id="container-2e7082e789ee952c6c92ca80807feec2"></div>
-    </div>
-  );
-}
-
-  return (
-    <div className="my-6 flex justify-center items-center">
-      <div id="container-2e7082e789ee952c6c92ca80807feec2"></div>
-    </div>
-  );
-}
-
-import { useState } from "react";
 
 export default function Home() {
   const [input, setInput] = useState("");
