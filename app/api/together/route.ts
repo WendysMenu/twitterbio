a0bc2500bebcc9376997fb8aae0ae28bc3b4913d
ABCD123 <anasanwer46@gmail.com>
2026-09-15 11:39:24 +0500

    Update route.ts

--- app/api/together/route.ts
+++ app/api/together/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: Request) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "groq/llama-3.3-70b-versatile",
+        model: "llama-3.1-8b-instant",
         messages: [
           {
             role: "system",
