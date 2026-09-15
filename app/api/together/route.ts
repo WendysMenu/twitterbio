25eb568691ff4c1322dedfc4095e635a78226c7d
ABCD123 <anasanwer46@gmail.com>
2026-09-15 11:55:03 +0500

    Update route.ts

--- app/api/together/route.ts
+++ app/api/together/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: Request) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "llama-3.3-70b-versatile",
+        model: "openai/gpt-oss-20b",
         messages: [
           {
             role: "system",
