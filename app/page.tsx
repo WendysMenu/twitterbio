f1a91f603c7133b91d5b9c5ace4debf48ca42523
ABCD123 <anasanwer46@gmail.com>
2026-09-15 16:13:23 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,20 @@
 import { useEffect } from "react";
 function NativeAd() {
   useEffect(() => {
+    // Check agar script pehle se script tag mein na ho
     const script = document.createElement('script');
     script.src = 'https://pl31351417.profitableratecpmnetwork.com/2e7082e789ee952c6c92ca80807feec2/invoke.js';
     script.async = true;
     script.setAttribute('data-cfasync', 'false');
     document.body.appendChild(script);
   }, []);
+
+  return (
+    <div className="my-6 flex justify-center items-center min-h-[90px]">
+      <div id="container-2e7082e789ee952c6c92ca80807feec2"></div>
+    </div>
+  );
+}
 
   return (
     <div className="my-6 flex justify-center items-center">
