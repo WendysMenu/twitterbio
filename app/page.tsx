aea57499971cd6c73048ad8182eea8dcf1fcded8
ABCD123 <anasanwer46@gmail.com>
2026-09-15 16:04:48 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
 "use client";
+function NativeAd() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://pl31351417.profitableratecpmnetwork.com/2e7082e789ee952c6c92ca80807feec2/invoke.js';
+    script.async = true;
+    script.setAttribute('data-cfasync', 'false');
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <div className="my-6 flex justify-center items-center">
+      <div id="container-2e7082e789ee952c6c92ca80807feec2"></div>
+    </div>
+  );
+}
 
 import { useState } from "react";
-import NativeAd from "@/components/NativeAd";
 
 export default function Home() {
   const [input, setInput] = useState("");
